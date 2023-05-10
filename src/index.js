cb493040f50e80a08d2c8f1289223bc37131494e
Anson Chan <anson0370@gmail.com>
2023-05-10 11:36:58 +0800

    add a high contrast version for light theme

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ console.log('Generating theme: high contrast');
 const highContrastColors = getColors((color, name) => {
   return color;
 });
-highContrastColors.colors.highContrastBlack = highContrastColors.originColors.black.clone().darken(3).toHexString();
+highContrastColors.colors.highContrastBackground = highContrastColors.originColors.background.clone().darken(3).toHexString();
 const highContrastTheme = getTheme('Hard Hacker High Contrast', highContrastColors, { highContrast: true });
 
 fs.writeFile('./themes/high-contrast.json', JSON.stringify(highContrastTheme, null, 2)).catch(() => process.exit(1));
@@ -37,19 +37,15 @@ fs.writeFile('./themes/high-contrast.json', JSON.stringify(highContrastTheme, nu
 // light themes
 console.log('Generating theme: light');
 const lightTheme = getTheme('Hard Hacker Light', getLightColors((color, name) => {
-  // switch (name) {
-  //   case 'red':
-  //   case 'green':
-  //   case 'blue':
-  //   case 'cyan':
-  //   case 'purple':
-  //   case 'yellow':
-  //   case 'orange':
-  //     color.brighten(-18);
-  //     break;
-  //   default:
-  // }
   return color;
 }));
 
 fs.writeFile('./themes/light.json', JSON.stringify(lightTheme, null, 2)).catch(() => process.exit(1));
+
+console.log('Generating theme: light hight contrast');
+const lightHighContrastColors = getLightColors((color, name) => {
+  return color;
+});
+lightHighContrastColors.colors.highContrastBackground = lightHighContrastColors.originColors.background.clone().darken(3).toHexString();
+const lightHighContrastTheme = getTheme('Hard Hacker Light High Contrast', lightHighContrastColors, { highContrast: true });
+fs.writeFile('./themes/light-high-contrast.json', JSON.stringify(lightHighContrastTheme, null, 2)).catch(() => process.exit(1));
